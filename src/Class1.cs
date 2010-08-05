67eae30e7e1330820f67fad7c3ebc318eecdc81d
Allen Hurst <akhurst@aggienetwork.com>
2010-08-05 10:13:20 -0500

    adding changes from TAMU Class

---
+++ src/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp4Demo
+{
+	public static class Extensions
+	{
+		public static string MyToString(this int i)
+		{
+			return "Number " + i;
+		}
+
+		public static string NullSafeToString(this string s)
+		{
+			if (s != null)
+			{
+				return s.ToString();
+			}
+			else
+			{
+				return "String is null";
+			}
+		}
+	}
+}
